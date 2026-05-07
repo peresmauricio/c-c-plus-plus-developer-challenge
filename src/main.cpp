8e08b5499144f10dbd65af248c4a21acab2c944b
peresmauricio <peresmauricio@gmailcom>
2026-05-07 03:56:11 -0500

    Improvement of log configuration

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include "app_matrix.h"
 #include <spdlog/spdlog.h>
 #include <spdlog/sinks/rotating_file_sink.h>
+#include <filesystem>
 
 
 using namespace std;
@@ -11,14 +12,29 @@ using namespace std;
 
 int main() {
 
+    std::filesystem::create_directories("logs");
     auto logger = spdlog::rotating_logger_mt(
-        "app",
-        "app.log",
+        "file_logger",
+        "logs/app.log",
         5 * 1024 * 1024, // 5 MB
         3               // 3 arquivos
     );
 
-    logger->info("Aplicação iniciada");
+
+    auto cpp_std = []() {
+        switch (__cplusplus) {
+            case 199711L: return "C++98/03";
+            case 201103L: return "C++11";
+            case 201402L: return "C++14";
+            case 201703L: return "C++17";
+            case 202002L: return "C++20";
+            case 202302L: return "C++23";
+            default:      return "Desconhecida";
+        }
+    }();
+
+    // Padrão {fmt}
+    logger->info("Aplicação iniciada. Padrão: {} (macro __cplusplus={})", cpp_std, __cplusplus);
     logger->warn("Algo estranho aconteceu");
     logger->error("Erro ao abrir arquivo");
 
