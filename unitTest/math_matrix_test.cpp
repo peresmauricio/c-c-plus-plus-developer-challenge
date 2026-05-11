b36cdeb953a7f16eae428edb56d9210fb33d3064
peresmauricio <peresmauricio@gmailcom>
2026-05-11 04:04:26 -0500

    Refactoring unit test

--- unitTest/math_matrix_test.cpp
+++ unitTest/math_matrix_test.cpp
@@ -1,11 +1,12 @@
 #include <gtest/gtest.h>
 #include "matrix.h"
+#include "lib_matrix.h"
 
 
 TEST(MatrixTest, Determinant2x2) {
     Matrix mat(2, 2);
     mat.at(0, 0) = 1; mat.at(0, 1) = 2;
     mat.at(1, 0) = 3; mat.at(1, 1) = 4;
-    EXPECT_DOUBLE_EQ(mat.determinant(), -2.0);
+    EXPECT_DOUBLE_EQ(Lib_matrix::determinant(mat), -2.0);
 }
 
